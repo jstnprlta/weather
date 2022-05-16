1ecb2bf2eac1c0d568d9270e6cbf06dcaf664c5c
Justin Peralta <justinperalta@Justins-MacBook-Air.local>
2022-05-15 20:26:20 -0500

    index.html

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var cities = any[e21b59c55c0dd3ae8d53325364e3bffc];
+
+var cityFormEl = document.querySelector("#city-search-form");
+var cityInputEl = document.querySelector("#city");
+var weatherContainerEl = document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
